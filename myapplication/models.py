150df9be2cc46c5f083420f2cf2fada34c5f5321
poovaa <145188081+poovaa@users.noreply.github.com>
2024-10-25 00:43:17 +0530

    Update models.py

--- myapplication/models.py
+++ myapplication/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+
+	name=models.TextField()
+	email=models.TextField()
+	password=models.TextField()
+	class Meta:
+		db_table='user'
